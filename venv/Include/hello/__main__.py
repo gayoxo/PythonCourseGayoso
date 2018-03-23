a4b91284ee8b5a555164be0ddd065e67a0cbf7a9
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2018-03-23 14:51:31 +0100

    Hola Hola Semana SantaX2
    Signed-off-by: Joaquin Gayoso Cabada <jgayoso@ucm.es>

--- venv/Include/hello/__main__.py
+++ venv/Include/hello/__main__.py
@@ -2,9 +2,13 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(prog="hello")
 parser.add_argument('-id','--user_id',type=int)
+parser.add_argument('type',type=str,choices=['true','false'])
+parser.add_argument('--opcional',type=str,default='opcional')
 
 args = parser.parse_args()
 
+print(args.type)
+print(args.opcional)
 # print(args.user_id)
 
 from day_17 import read_data
